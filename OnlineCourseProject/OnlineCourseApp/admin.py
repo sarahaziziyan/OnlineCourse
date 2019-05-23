f64c0ed07c4cc1b1329631a34358a3f75881a6c8
Sarah Aziziyan <aziziyan.sarah@gmail.com>
2019-05-23 13:25:02 +0430

    admin added

--- OnlineCourseProject/OnlineCourseApp/admin.py
+++ OnlineCourseProject/OnlineCourseApp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.contrib.sessions.models import Session
+from .models import *
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(Course)
+admin.site.register(Users)
+admin.site.register(Instructor)
+admin.site.register(InstructorCourse)
--- OnlineCourseProject/OnlineCourseProject/urls.py
+++ OnlineCourseProject/OnlineCourseProject/urls.py
@@ -17,5 +17,10 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin', admin.site.urls),
 ]
+
+from . import settings
+from django.contrib.staticfiles.urls import static
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
