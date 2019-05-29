05ca7fe8bc6b97288351f32a556d7bc7ddac33db
Sarah Aziziyan <aziziyan.sarah@gmail.com>
2019-05-29 18:25:08 +0430

    OnlineCourse  - fixed customUser bug  - created the sign_up form and developed view for sign_up

--- OnlineCourseProject/OnlineCourseApp/views.py
+++ OnlineCourseProject/OnlineCourseApp/views.py
@@ -1,10 +1,26 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import CustomUser
+from django.contrib.auth.models import User
+
 
 def login(request):
     return render(request, "login.html", {})
 
 
 def sign_up(request):
+    user_ins = User(
+        first_name=request.POST['first_name'],
+        last_name=request.POST['last_name'],
+        email=request.POST['email'],
+        username=request.POST['username'],
+        password=request.POST['password'],
+    )
+    user_ins.save()
+    custom_user_ins = CustomUser(
+        user=User.objects.get(username=request.POST['username']),
+        nationalCode=request.POST['national_code'],
+    )
+    custom_user_ins.save()
     return render(request, "dashboard.html", {});
