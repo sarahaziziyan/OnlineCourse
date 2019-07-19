e4ddc0d714bc438f2943868936140a7bb891f5c1
Sarah Aziziyan <aziziyan.sarah@yahoo.com>
2019-07-19 17:51:33 +0430

    edited date field in courseForm

--- OnlineCourseProject/OnlineCourseApp/forms.py
+++ OnlineCourseProject/OnlineCourseApp/forms.py
@@ -1,13 +1,16 @@
 from django import forms
 from django.contrib.auth.models import User
-from django.forms import ModelForm
+from django.forms import ModelForm ,DateInput
 from .models import CustomUser, Course
 
+class MyDateInput(DateInput):
+    input_type = 'date'
 
 class CourseForm(ModelForm):
     class Meta:
         fields = '__all__'
         model = Course
+        widgets = {'creation_date': MyDateInput()}
 
 
 class UserForm(ModelForm):
